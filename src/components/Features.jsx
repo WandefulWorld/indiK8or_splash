5d1f2a20c80c3c77c86700ef088425a69026c4d1
Ivy Wang <wanwangbtb@gmail.com>
2023-08-08 19:57:56 -0400

    completed github component

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -44,8 +44,8 @@ export default function Features() {
     <div className='bg-black font-serif '>
       <div className="mx-auto grid max-w-2xl grid-cols-1 items-center gap-x-8 gap-y-16 px-4 py-24 sm:px-6 sm:py-32 lg:max-w-7xl lg:grid-cols-1 lg:px-8">
         <div>
-          <div className='w-12 h-12 p-[0.1875rem] rounded-full ring-1 ring-slate-900/10 shadow overflow-hidden dark:bg-sky-500 dark:highlight-white/30'></div>
-          <h2 className="mt-4 text-3xl font-bold tracking-tight text-sky-500 sm:text-4xl">Technical Specifications</h2>
+          <div className='w-12 h-12 p-[0.1875rem] rounded-full ring-1 ring-slate-900/10 shadow overflow-hidden dark:bg-pink-500 dark:highlight-white/30'></div>
+          <h2 className="mt-4 text-3xl font-bold tracking-tight text-gray-200 sm:text-4xl">Technical Specifications</h2>
           <p className="mt-4 text-gray-400">
            INDIK8OR provides a user-friendly interface for developers to set up and interact with their Kubernetes clusters. By leveraging Kubernetes orchestration for
            optimal scalability and effiency, INDIK8OR is able to provide real-time monitoring of the various architectural layers of the cluster.
@@ -54,7 +54,7 @@ export default function Features() {
           <dl className="mt-8 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 sm:gap-y-16 lg:gap-x-8">
             {features1.map((feature) => (
               <div key={feature.name} className="border-t border-gray-200 pt-4">
-                <dt className="font-bold text-sky-500">{feature.name}</dt>
+                <dt className="font-bold text-gray-400">{feature.name}</dt>
                 <dd className="mt-2 text-sm text-gray-400">{feature.description}</dd>
               </div>
             ))}
@@ -69,7 +69,7 @@ export default function Features() {
             ))}
             {features2.map((feature) => (
               <div key={feature.name} className="border-t border-gray-200 pt-4">
-                <dt className="font-bold text-sky-500">{feature.name}</dt>
+                <dt className="font-bold text-gray-400">{feature.name}</dt>
                 <dd className="mt-2 text-sm text-gray-400">{feature.description}</dd>
               </div>
             ))}
--- src/components/Github.jsx
+++ src/components/Github.jsx
@@ -1,8 +1,23 @@
-import github from '../assets/github.png'
 const Github = () => {
   return (
-    <div className='bg-black'>
-      {/* <img className=' container-center w-48 h-36' src={github} alt='github'/> */}
+    <div className='max-w-7xl mx-auto px-4 sm:px-6 md:px-8 font-serif'>
+      <div className='w-12 h-12 p-[0.1875rem] rounded-full ring-1 ring-slate-900/10 shadow overflow-hidden dark:bg-indigo-500 dark:highlight-white/20'>
+
+      </div>
+      <h2 className='mt-4 text-3xl font-bold tracking-tight text-gray-200 sm:text-4xl'>Visit Github Repository</h2>
+      <div className='mt-8 max-w-3xl space-y-6'>
+        <p>Check out INDIK8OR on our Github where you will find everything you need to start the application.</p>
+        <p>Feel free to submit any issues or contribute as a fellow developer to the open source product on Github.</p>
+      </div>
+      <a className='group inline-flex items-center h-9 rounded-full text-lg font-semibold whitespace-nowrap px-5 focus:outline-none focus:ring-2 bg-indigo-50 text-gray-200
+      hover:bg-indigo-500 hover:text-indigo-700 focus:ring-indigo-500 dark:bg-slate-700 dark:text-slate-slate-100
+      dark:hover:text-white dark:focus:ring-slate-500 mt-8' href="https://github.com/oslabs-beta/indiK8or">Get Started
+        <span className='sr-only'>, responsive design</span>
+        <svg className='overflow-visible ml-4 text-indigo-300 group-hover:text-indigo-400 dark:text-s;ate-500 dark:group-hover:text-slate-400
+        ' width='3' height='6' viewBox='0 0 3 6' fill='none' stroke='currentColor' strokeWidth='2' strokeLinecap='round' strokeLinejoin='round'>
+          <path d='M0 0L3 3L0 6'></path>
+        </svg>
+      </a>
     </div>
   )
 }
