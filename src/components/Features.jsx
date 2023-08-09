908f3d38f290f8ba9fb9f4b9cf916288edf5b71d
Ivy Wang <wanwangbtb@gmail.com>
2023-08-08 20:02:10 -0400

    updated feature amount

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,12 +1,8 @@
-const features1 = [
+const features = [
   { name: 'Visual Overview', description: 'It provides a visual overview including information on the nodes, pods, services, and other critical components of the cluster. With this information, administrators can quickly detect and respond to any issues such as node or pod failures, before they cause major problems.' },
-  { name: 'Automated Configuration Process', description: 'It incorporates Prometheus and Grafana into the Kubernetes cluster. With just a few steps users can set up their Prometheus and Grafana instances, making it easy to monitor and optimize the performance of their applications'},
-];
-const features2 = [
   { name: 'Pod Health', description: "It shows you the health and status of the pods, providing the details including image's name, ready, status, age, and IP. It additionally offers images & vulnerability scan." },
-  { name: 'Vulnerabilities Descriptions', description: 'You will be able to see the vulnerabilities with the ID and description. Each ID is a hyperlink that will bring you to a page that shows a more detailed description of each vulnerability' },
 ];
-const images1 = [
+const images = [
   {
     id: 1,
     name: 'screenshot1',
@@ -15,27 +11,11 @@ const images1 = [
     imageAlt: 'overview',
   },
   {
-    id: 2,
-    name: 'screenshot2',
-    href: '#',
-    imageSrc: 'https://miro.medium.com/v2/resize:fit:720/format:webp/1*Na2oghMXqMcCMoj0JIMskA.png',
-    imageAlt: 'automated configuration',
-  },
-];
-const images2 = [
-  {
     id: 3,
     name: 'screenshot3',
     href: '#',
     imageSrc: 'https://miro.medium.com/v2/resize:fit:720/format:webp/1*dpyiEcATS0FnWXXHqmXOew.png',
     imageAlt: 'pod',
-  },
-  {
-    id: 4,
-    name: 'screenshot4',
-    href: '#',
-    imageSrc: 'https://miro.medium.com/v2/resize:fit:720/format:webp/1*BRjqgA-9CLOrLlOlDSqISA.png',
-    imageAlt: 'vulnerabilities descriptions',
   },
 ];
 
@@ -52,28 +32,13 @@ export default function Features() {
           </p>
 
           <dl className="mt-8 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 sm:gap-y-16 lg:gap-x-8">
-            {features1.map((feature) => (
+            {features.map((feature) => (
               <div key={feature.name} className="border-t border-gray-200 pt-4">
                 <dt className="font-bold text-gray-400">{feature.name}</dt>
                 <dd className="mt-2 text-sm text-gray-400">{feature.description}</dd>
               </div>
             ))}
-            {images1.map((product) => (
-              <div key={product.id} className="group relative">
-                  <img
-                    src={product.imageSrc}
-                    alt={product.imageAlt}
-                    className="w-full object-fill object-center lg:h-full lg:w-full rounded-lg"
-                  />
-              </div>  
-            ))}
-            {features2.map((feature) => (
-              <div key={feature.name} className="border-t border-gray-200 pt-4">
-                <dt className="font-bold text-gray-400">{feature.name}</dt>
-                <dd className="mt-2 text-sm text-gray-400">{feature.description}</dd>
-              </div>
-            ))}
-            {images2.map((product) => (
+            {images.map((product) => (
               <div key={product.id} className="group relative">
                   <img
                     src={product.imageSrc}
